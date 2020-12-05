1106e23aa9e789167dad03b75453158410f56bca
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2020-12-05 16:11:57 -0500

    testing CI

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World! CI
+          Hello World! CI again
         </a>
       </header>
     </div>
